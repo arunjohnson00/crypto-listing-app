1bae6f2667dcabb9acbdb65a0037b4cddf3a2c3e
arunjohnson00 <arunjohnson00@gmail.com>
2022-06-02 17:32:21 +0530

    Tab Fixed Responsive

--- frontend/src/components/desktop/discoververticaltab/DiscoverVerticalTab.tsx
+++ frontend/src/components/desktop/discoververticaltab/DiscoverVerticalTab.tsx
@@ -196,7 +196,7 @@ const DiscoverVerticalTab = () => {
           }}
         />
       </Tabs>
-      <Box>
+      <Box sx={{ width: "80%" }}>
         <TabPanel value={value} index={0}>
           <DiscoverLatest />
         </TabPanel>
