bd54f9569ebeec2e78df0c07c0031ead7cb27df2
arunjohnson00 <arunjohnson00@gmail.com>
2022-07-27 12:27:11 +0530

    TopBar Button indicator changed

--- backend/src/components/button/notification/style.ts
+++ backend/src/components/button/notification/style.ts
@@ -3,8 +3,9 @@ import Badge, { BadgeProps } from "@mui/material/Badge";
 
 export const StyledBadge = styled(Badge)<BadgeProps>(({ theme }) => ({
   "& .MuiBadge-badge": {
-    right: 6,
-    top: 9,
+    right: 5,
+    backgroundColor: "#D8B9C3",
+    top: 8,
     border: `2px solid ${theme.palette.background.paper}`,
     padding: "7px 7px",
     borderRadius: "50px",
--- backend/src/components/button/settings/style.ts
+++ backend/src/components/button/settings/style.ts
@@ -3,8 +3,9 @@ import Badge, { BadgeProps } from "@mui/material/Badge";
 
 export const StyledBadge = styled(Badge)<BadgeProps>(({ theme }) => ({
   "& .MuiBadge-badge": {
-    right: 6,
-    top: 9,
+    right: 5,
+    backgroundColor: "#D6DCB4",
+    top: 8,
     border: `2px solid ${theme.palette.background.paper}`,
     padding: "7px 7px",
     borderRadius: "50px",
