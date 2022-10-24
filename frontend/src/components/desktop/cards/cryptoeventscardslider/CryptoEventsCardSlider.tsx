2648af0fbd8314558d577ae7cd5d5913b79c792a
arunjohnson00 <arunjohnson00@gmail.com>
2022-10-24 11:07:59 +0530

    EventSlider Added

--- frontend/src/components/desktop/cards/cryptoeventscardslider/CryptoEventsCardSlider.tsx
+++ frontend/src/components/desktop/cards/cryptoeventscardslider/CryptoEventsCardSlider.tsx
@@ -35,62 +35,76 @@ const CryptoEventsCardSlider = ({ data, variant }: any) => {
             backgroundColor: variant === "ads" ? "#021250" : "#01061A",
             border: "1px solid #020C37",
             borderRadius: 4,
+            minHeight: 150,
+            maxHeight: 150,
           }}
           px={2}
           py={1.5}
           mx={1}
         >
-          <Stack direction="row" spacing={2} sx={{ alignItems: "flex-start" }}>
-            <Avatar
-              sx={{ width: 50, height: 50, mt: 1 }}
-              variant="square"
-              src={`${serverAPIUrl}public/uploads/event_proof/${data?.coin_logo}`}
-              alt={data && data?.title}
-            />
-            <Stack direction="column" spacing={0.8} width="100%">
-              <Stack direction="column" spacing={0} width="100%">
-                <Typography
-                  sx={{
-                    color: "#FFFFFF",
-                    fontSize: "1.1rem",
-                    fontWeight: 500,
-                  }}
-                >
-                  {data && data?.title}.
-                </Typography>
-                <Typography
-                  sx={{
-                    color: "#FFFFFF",
-                    fontSize: ".75rem",
-                    fontWeight: 400,
-                  }}
-                >
-                  {data && data?.description?.length >= 70
-                    ? data && data?.description.slice(0, 70) + "..."
-                    : data && data?.description}
-                </Typography>
-                <Typography
-                  sx={{
-                    color: "#A8CDE9",
-                    fontSize: ".9rem",
-                    fontWeight: 500,
-                  }}
-                >
-                  {data &&
-                    moment(new Date(data?.event_date)).format("DD MMM  YYYY")}
-                </Typography>
-              </Stack>
+          <Stack direction="column" justifyContent="space-between" height={150}>
+            <Stack
+              direction="row"
+              spacing={2}
+              sx={{ alignItems: "flex-start" }}
+            >
+              <Avatar
+                sx={{ width: 50, height: 50, mt: 1 }}
+                variant="square"
+                src={`${serverAPIUrl}public/uploads/event_proof/${data?.coin_logo}`}
+                alt={data && data?.title}
+              />
               <Stack
-                direction="row"
+                direction="column"
                 spacing={0.8}
                 width="100%"
-                sx={{
-                  alignItems: "center",
-                  justifyContent:
-                    variant === "ads" ? "space-between" : "flex-end",
-                }}
+                height="100%"
               >
-                {/* {variant === "ads" && (
+                <Stack direction="column" spacing={0} width="100%">
+                  <Typography
+                    sx={{
+                      color: "#FFFFFF",
+                      fontSize: "1.1rem",
+                      fontWeight: 500,
+                    }}
+                  >
+                    {data && data?.title}.
+                  </Typography>
+                  <Typography
+                    sx={{
+                      color: "#FFFFFF",
+                      fontSize: ".75rem",
+                      fontWeight: 400,
+                    }}
+                  >
+                    {data && data?.description?.length >= 70
+                      ? data && data?.description.slice(0, 70) + "..."
+                      : data && data?.description}
+                  </Typography>
+                  <Typography
+                    sx={{
+                      color: "#A8CDE9",
+                      fontSize: ".9rem",
+                      fontWeight: 500,
+                    }}
+                  >
+                    {data &&
+                      moment(new Date(data?.event_date)).format("DD MMM  YYYY")}
+                  </Typography>
+                </Stack>
+              </Stack>
+            </Stack>
+            <Stack
+              direction="row"
+              spacing={0.8}
+              width="100%"
+              sx={{
+                alignItems: "flex-end",
+                justifyContent:
+                  variant === "ads" ? "space-between" : "flex-end",
+              }}
+            >
+              {/* {variant === "ads" && (
                   <Button
                     variant="contained"
                     startIcon={<ThumbUpOffAltIcon />}
@@ -107,21 +121,20 @@ const CryptoEventsCardSlider = ({ data, variant }: any) => {
                   </Button>
                 )} */}
 
-                <Button
-                  variant="contained"
-                  startIcon={<AccessTimeIcon />}
-                  size="small"
-                  sx={{
-                    borderRadius: 10,
-                    textTransform: "inherit",
-                    fontSize: ".6rem",
-                    color: "#FFFFFF",
-                    backgroundColor: "#2415A2",
-                  }}
-                >
-                  {data && moment(new Date(data?.event_date)).fromNow()}
-                </Button>
-              </Stack>
+              <Button
+                variant="contained"
+                startIcon={<AccessTimeIcon />}
+                size="small"
+                sx={{
+                  borderRadius: 10,
+                  textTransform: "inherit",
+                  fontSize: ".6rem",
+                  color: "#FFFFFF",
+                  backgroundColor: "#2415A2",
+                }}
+              >
+                {data && moment(new Date(data?.event_date)).fromNow()}
+              </Button>
             </Stack>
           </Stack>
         </Box>
--- frontend/src/components/mobile/cards/cryptoeventscardslider/MobileCryptoEventsCardSlider.tsx
+++ frontend/src/components/mobile/cards/cryptoeventscardslider/MobileCryptoEventsCardSlider.tsx
@@ -26,7 +26,7 @@ const MobileCryptoEventsCardSlider = ({ data, variant }: any) => {
 
   return (
     <Grid item xs={12}>
-      <Stack direction="column">
+      <Stack direction="column" my={0.5}>
         <Box
           sx={{
             flexGrow: 1,
@@ -35,82 +35,110 @@ const MobileCryptoEventsCardSlider = ({ data, variant }: any) => {
             backgroundColor: variant === "ads" ? "#021250" : "#01061A",
             border: "1px solid #020C37",
             borderRadius: 4,
+            // minHeight: 150,
+            // maxHeight: 150,
           }}
           px={2}
           py={1.5}
           mx={1}
         >
-          <Stack direction="row" spacing={2} sx={{ alignItems: "flex-start" }}>
-            <Avatar
-              sx={{ width: 50, height: 50 }}
-              variant="square"
-              src={`${serverAPIUrl}public/uploads/event_proof/${data?.coin_logo}`}
-              alt={data && data?.title}
-            />
-            <Stack direction="column" spacing={0.8} width="100%">
-              <Stack direction="column" spacing={0} width="100%">
-                <Typography
-                  sx={{
-                    color: "#FFFFFF",
-                    fontSize: "1.1rem",
-                    fontWeight: 500,
-                  }}
-                >
-                  {data && data?.title}
-                </Typography>
-                <Typography
-                  sx={{
-                    color: "#A8CDE9",
-                    fontSize: ".9rem",
-                    fontWeight: 500,
-                  }}
-                >
-                  {data &&
-                    moment(new Date(data?.event_date)).format("DD MMM  YYYY")}
-                </Typography>
-              </Stack>
+          <Stack
+            direction="column"
+            justifyContent="space-between"
+            height={"auto"}
+          >
+            <Stack
+              direction="row"
+              spacing={2}
+              sx={{ alignItems: "flex-start" }}
+            >
+              <Avatar
+                sx={{ width: 50, height: 50, mt: 1 }}
+                variant="square"
+                src={`${serverAPIUrl}public/uploads/event_proof/${data?.coin_logo}`}
+                alt={data && data?.title}
+              />
               <Stack
-                direction="row"
+                direction="column"
                 spacing={0.8}
                 width="100%"
-                sx={{
-                  alignItems: "center",
-                  justifyContent:
-                    variant === "ads" ? "space-between" : "flex-end",
-                }}
+                height="100%"
               >
-                {/* {variant === "ads" && (
-                  <Button
-                    variant="contained"
-                    startIcon={<ThumbUpOffAltIcon />}
-                    size="small"
+                <Stack direction="column" spacing={0} width="100%">
+                  <Typography
                     sx={{
-                      borderRadius: 10,
-                      textTransform: "capitalize",
-                      fontSize: ".6rem",
-                      color: "#00BEAA",
-                      backgroundColor: "#01061A",
+                      color: "#FFFFFF",
+                      fontSize: "1.1rem",
+                      fontWeight: 500,
                     }}
                   >
-                    238 Votes
-                  </Button>
-                )} */}
-
+                    {data && data?.title}.
+                  </Typography>
+                  <Typography
+                    sx={{
+                      color: "#FFFFFF",
+                      fontSize: ".75rem",
+                      fontWeight: 400,
+                    }}
+                  >
+                    {data && data?.description?.length >= 70
+                      ? data && data?.description.slice(0, 70) + "..."
+                      : data && data?.description}
+                  </Typography>
+                  <Typography
+                    sx={{
+                      color: "#A8CDE9",
+                      fontSize: ".9rem",
+                      fontWeight: 500,
+                    }}
+                  >
+                    {data &&
+                      moment(new Date(data?.event_date)).format("DD MMM  YYYY")}
+                  </Typography>
+                </Stack>
+              </Stack>
+            </Stack>
+            <Stack
+              direction="row"
+              spacing={0.8}
+              width="100%"
+              sx={{
+                alignItems: "flex-end",
+                justifyContent:
+                  variant === "ads" ? "space-between" : "flex-end",
+              }}
+            >
+              {/* {variant === "ads" && (
                 <Button
                   variant="contained"
-                  startIcon={<AccessTimeIcon />}
+                  startIcon={<ThumbUpOffAltIcon />}
                   size="small"
                   sx={{
                     borderRadius: 10,
-                    textTransform: "inherit",
+                    textTransform: "capitalize",
                     fontSize: ".6rem",
-                    color: "#FFFFFF",
-                    backgroundColor: "#2415A2",
+                    color: "#00BEAA",
+                    backgroundColor: "#01061A",
                   }}
                 >
-                  {data && moment(new Date(data?.event_date)).fromNow()}
+                  238 Votes
                 </Button>
-              </Stack>
+              )} */}
+
+              <Button
+                variant="contained"
+                startIcon={<AccessTimeIcon />}
+                size="small"
+                sx={{
+                  borderRadius: 10,
+                  textTransform: "inherit",
+                  fontSize: ".6rem",
+                  color: "#FFFFFF",
+                  backgroundColor: "#2415A2",
+                }}
+              >
+                {data && moment(new Date(data?.event_date)).fromNow()}
+              </Button>
             </Stack>
           </Stack>
         </Box>
